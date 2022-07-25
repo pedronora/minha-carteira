9de55b0996d5a5034aaf83b6aaf9cd427343b94b
pedronora <pedro@nora.vc>
2022-07-25 10:06:30 -0300

    Admin Operacao Model Register

--- acao/admin.py
+++ acao/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Acao
+from .models import Acao, Operacao
 
 # Register your models here.
 admin.site.register(Acao)
+admin.site.register(Operacao)
--- fii/admin.py
+++ fii/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import FII
+from .models import FII, Operacao
 
 # Register your models here.
 admin.site.register(FII)
+admin.site.register(Operacao)

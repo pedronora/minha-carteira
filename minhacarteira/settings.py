969b3dbbf3e3f24e9186d25fde502725e49d85ff
pedronora <pedro@nora.vc>
2022-08-05 09:42:55 -0300

    settings.py production

--- minhacarteira/settings.py
+++ minhacarteira/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['minha-carteira.azurewebsites.net', '127.0.0.1']
+ALLOWED_HOSTS = ['minha-carteira.azurewebsites.net']
 
 CSRF_TRUSTED_ORIGINS = ['https://minha-carteira.azurewebsites.net']
 
@@ -93,7 +93,7 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
-        # 'OPTIONS': {'sslmode': 'require'}
+        'OPTIONS': {'sslmode': 'require'}
     }
 }
 
